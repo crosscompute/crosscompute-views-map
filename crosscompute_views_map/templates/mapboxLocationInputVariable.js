1b4f380b6ef47cc28003ba3de04cadb265c52ffc
Roy Hyunjin Han <rhh@crosscompute.com>
2022-08-23 18:59:48 -0400

    Use setupMapMapbox

--- crosscompute_views_map/templates/mapboxLocationInputVariable.js
+++ crosscompute_views_map/templates/mapboxLocationInputVariable.js
@@ -1,4 +1,4 @@
-const {{ element_id }} = addMapboxControls(new mapboxgl.Map({{ map }}));
+const {{ element_id }} = setupMapMapbox(new mapboxgl.Map({{ map }}), '{{ element_id }}');
 const {{ element_id }}_marker = centerMarker(new mapboxgl.Marker(), {{ element_id }}).addTo({{ element_id }});
 {{ element_id }}.on('move', () => {
   centerMarker({{ element_id }}_marker, {{ element_id }});

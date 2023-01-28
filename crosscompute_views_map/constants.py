a137d66af3fc5d7980f920d4b487c9a045712bf0
Roy Hyunjin Han <rhh@crosscompute.com>
2023-01-27 20:36:29 -0500

    Bump mapbox version

--- crosscompute_views_map/constants.py
+++ crosscompute_views_map/constants.py
@@ -5,8 +5,8 @@ PACKAGE_FOLDER = Path(__file__).parent
 TEMPLATES_FOLDER = PACKAGE_FOLDER / 'templates'
 
 
-MAPBOX_CSS_URI = 'https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css'
-MAPBOX_JS_URI = 'https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'
+MAPBOX_CSS_URI = 'https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.css'
+MAPBOX_JS_URI = 'https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.js'
 
 
 TURF_JS_URI = (
